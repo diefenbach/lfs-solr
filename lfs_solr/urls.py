ff2827de47751687fc8c024404bbc812f698b1f5
Kai Diefenbach <kai.diefenbach@iqpp.de>
2010-07-03 22:20:15 +0200

    Make urls lfs_solr specific

--- lfs_solr/urls.py
+++ lfs_solr/urls.py
@@ -4,9 +4,9 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('lfs_solr.views',
     url(r'^search', "search", name="solr_search"),
     url(r'^livesearch', "livesearch", name="solr_livesearch"),
-    url(r'^set-filter', "set_filter", name="solr_set_filter"),
-    url(r'^set-sorting', "set_sorting", name="solr_set_sorting"),
-    url(r'^reset-filter', "reset_filter", name="solr_reset_filter"),
-    url(r'^reset-field', "reset_field", name="solr_reset_field"),
+    url(r'^solr-set-filter', "set_filter", name="solr_set_filter"),
+    url(r'^solr-set-sorting', "set_sorting", name="solr_set_sorting"),
+    url(r'^solr-reset-filter', "reset_filter", name="solr_reset_filter"),
+    url(r'^solr-reset-field', "reset_field", name="solr_reset_field"),
     url(r'^index-products', "index_products", name="solr_index_products"),
 )
